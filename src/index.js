fe8289bd5ba7cfa9db0b85a6ef32d185ff62f2af
FlightLoong <1577886278@qq.com>
2020-04-17 00:29:41 +0800

    调整标题和外观

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
 import App from './App.jsx'
 
 // 导入 antd-mobile 样式
 import 'antd-mobile/dist/antd-mobile.css'
+
+// 导入全局样式文件
+// 我们写的样式需要放到导入的样式库后面，这样才会生效，我们自己的样式才会覆盖样式库的样式
+import './index.css'
 
 ReactDOM.render(
   <React.StrictMode>

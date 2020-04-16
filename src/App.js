3b886f9987f08904aef2d315b668e71c0a5c58f4
FlightLoong <1577886278@qq.com>
2020-04-17 00:00:21 +0800

    导入antd组件库

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
-import React from 'react';
+import React from 'react'
+// 导入组件
+import { Button } from 'antd-mobile'
+// 导入 antd-mobile 样式
+import 'antd-mobile/dist/antd-mobile.css'
 
 function App() {
   return (
-    <div>根组件</div>
+    <div>
+      <p>根组件</p>
+      {/* 使用 antd-mobile 中的 Button 组件 */}
+      <Button type="warning" disabled>warning disabled</Button>
+    </div>
   )
 }
 

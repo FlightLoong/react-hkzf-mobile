f0c00f30b7bae047465ad2fd65d50549feceb3b6
FlightLoong <1577886278@qq.com>
2020-04-18 23:24:09 +0800

    获取城市列表数据

--- src/pages/CityList/index.jsx
+++ src/pages/CityList/index.jsx
@@ -3,10 +3,23 @@ import React from 'react'
 // 导入 NavBar组件
 import { NavBar } from 'antd-mobile'
 
+// 导入 axios
+import axios from 'axios'
+
 // 导入样式
 import './index.scss'
 
 export default class CityList extends React.Component {
+  componentDidMount() {
+    this.getCityList()
+  }
+
+  // 获取城市列表数据的方法
+  async getCityList() {
+    const { data: res } = await axios.get('http://118.190.160.53:8009/area/city?level=1')
+    console.log(res)
+  }
+
   render() {
     return (
       <div className="citylist">

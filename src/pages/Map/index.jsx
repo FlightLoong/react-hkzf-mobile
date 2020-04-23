bc80c0b14df547a7cab38e7d7788f9ec1ec918cb
FlightLoong <1577886278@qq.com>
2020-04-24 01:04:30 +0800

    根据定位展示当前城市

--- src/pages/Map/index.jsx
+++ src/pages/Map/index.jsx
@@ -7,16 +7,33 @@ import NavHeader from '../../components/NavHeader/index.jsx'
 // import './index.scss'
 import styles from './index.module.css'
 
+// 声明一个全局地图变量
+const BMap = window.BMap
+
 export default class Map extends React.Component {
   componentDidMount() {
+    // 获取当前定位城市
+    const { label, value } = JSON.parse(localStorage.getItem('hkzf_city'))
     // 初始化实例
-    const map = new window.BMap.Map("container")
+    const map = new BMap.Map("container")
 
     // 初始化中心点
-    const point = new window.BMap.Point(116.404, 39.915)
+    // const point = new BMap.Point(116.404, 39.915)
 
-    // 地图初始化，同时设置地图展示级别
-    map.centerAndZoom(point, 15)
+    // 创建地址解析器实例     
+    const myGeo = new BMap.Geocoder()
+    // 将地址解析结果显示在地图上，并调整地图视野    
+    myGeo.getPoint(label, function (point) {
+      if (point) {
+        // 地图初始化，同时设置地图展示级别
+        map.centerAndZoom(point, 11)
+        // map.addOverlay(new BMap.Marker(point))
+
+        // 添加控件
+        map.addControl(new BMap.NavigationControl())
+        map.addControl(new BMap.ScaleControl())
+      }
+    }, label)
   }
 
   render() {

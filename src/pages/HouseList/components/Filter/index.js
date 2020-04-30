17f0891e58620312853387068b0aab2f3b372a25
FlightLoong <1577886278@qq.com>
2020-04-30 08:17:51 +0800

    FilterTitle组件实现

--- src/pages/HouseList/components/Filter/index.js
+++ src/pages/HouseList/components/Filter/index.js
@@ -6,8 +6,36 @@ import FilterMore from '../FilterMore'
 
 import styles from './index.module.css'
 
+// 标题高亮状态
+// true 表示高亮； false 表示不高亮
+const titleSelectedStatus = {
+  area: true,
+  mode: false,
+  price: false,
+  more: false
+}
+
 export default class Filter extends Component {
+
+  state = {
+    titleSelectedStatus
+  }
+
+  // 点击标题菜单实现高亮
+  onTitleClick = (type) => {
+    // console.log(type)
+    this.setState(prevState => {
+      return {
+        titleSelectedStatus: {
+          ...prevState.titleSelectedStatus,
+          [type]: true
+        }
+      }
+    })
+  }
+
   render() {
+    const { titleSelectedStatus } = this.state
     return (
       <div className={styles.root}>
         {/* 前三个菜单的遮罩层 */}
@@ -15,7 +43,7 @@ export default class Filter extends Component {
 
         <div className={styles.content}>
           {/* 标题栏 */}
-          <FilterTitle />
+          <FilterTitle titleSelectedStatus={titleSelectedStatus} onClick={this.onTitleClick} />
 
           {/* 前三个菜单对应的内容： */}
           {/* <FilterPicker /> */}
